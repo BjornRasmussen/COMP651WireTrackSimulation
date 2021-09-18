bb828eefd6596fe64e4061c4bcc7d447314a89c6
Leif <354lbr@gmail.com>
2021-09-18 19:37:17 -0400

    Better track

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -4,6 +4,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GraphicsPanel extends JFrame {
+    Point red;
+    Point green;
     private Track _t;
     public GraphicsPanel(Track t) {
         _t = t;
@@ -11,6 +13,7 @@ public class GraphicsPanel extends JFrame {
         setSize(800, 800);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBackground(Color.WHITE);
 
         addMouseListener(new MouseListener() {
             @Override
@@ -41,9 +44,9 @@ public class GraphicsPanel extends JFrame {
 
         new Thread(() -> {
             for (int i = 0; i < 10000 /*10000 = 5 min */; i++) {
-                _t.moveCar(0.015);
+                _t.moveCar(0.07);
                 try {
-                    Thread.sleep(40);
+                    Thread.sleep(30);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -54,8 +57,15 @@ public class GraphicsPanel extends JFrame {
 
     @Override
     public void paint(Graphics g1) {
-        super.paintComponents(g1);
         Graphics2D g = (Graphics2D) g1;
+
+        if (red != null) {
+            g.setColor(Color.WHITE);
+            g.setStroke(new BasicStroke(25, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 10));
+            g.drawLine(red.x, red.y, red.x, red.y);
+            g.drawLine(green.x, green.y, green.x, green.y);
+        }
+
         g.setStroke(new BasicStroke(4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 10));
         g.setColor(new Color(40, 170, 242));
         int[] xArray = new int[_t.getPoints().size()+1];
@@ -69,17 +79,20 @@ public class GraphicsPanel extends JFrame {
         xArray[xArray.length-1] = xArray[0];
         yArray[yArray.length-1] = yArray[0];
         g.drawPolyline(xArray, yArray, xArray.length);
-        g.setStroke(new BasicStroke(15, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 10));
+        g.setStroke(new BasicStroke(25, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 10));
         g.setColor(Color.RED);
         DoublePoint magnet = _t.getMagnetPos();
         Point mp = convertCoords(magnet);
         g.drawLine(mp.x, mp.y, mp.x, mp.y);
 
-        g.setStroke(new BasicStroke(13, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 10));
+        g.setStroke(new BasicStroke(23, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 10));
         g.setColor(Color.GREEN);
         DoublePoint car = _t.getCarPos();
         Point cp = convertCoords(car);
         g.drawLine(cp.x, cp.y, cp.x, cp.y);
+
+        red = mp;
+        green = cp;
     }
 
     private static Point convertCoords(DoublePoint p) {
--- src/main.java
+++ src/main.java
@@ -4,21 +4,26 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         List<DoublePoint> points = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            double angleFromTop = i*Math.PI/5;
-            double distsmall = 5;
-            double distbetween = 6.95;
-            double distbig = 7;
-            double ang2 = 0.17;
-            double ang3 = 0.4;
-            double ang4 = 0.42;
-            points.add(new DoublePoint(distsmall*Math.sin(angleFromTop), distsmall*Math.cos(angleFromTop)));
-            points.add(new DoublePoint(distbetween*Math.sin(angleFromTop+ang2), distbetween*Math.cos(angleFromTop+ang2)));
-            points.add(new DoublePoint(distbig*Math.sin(angleFromTop+ang3), distbig*Math.cos(angleFromTop+ang3)));
-            points.add(new DoublePoint(distsmall*Math.sin(angleFromTop+ang4), distsmall*Math.cos(angleFromTop+ang4)));
+//        for (int i = 0; i < 10; i++) {
+//            double angleFromTop = i*Math.PI/5;
+//            double distsmall = 5;
+//            double distbetween = 6.9;
+//            double distbig = 7;
+//            double ang2 = 0.17;
+//            double ang3 = 0.4;
+//            double ang4 = 0.42;
+//            points.add(new DoublePoint(distsmall*Math.sin(angleFromTop), distsmall*Math.cos(angleFromTop)));
+//            points.add(new DoublePoint(distbetween*Math.sin(angleFromTop+ang2), distbetween*Math.cos(angleFromTop+ang2)));
+//            points.add(new DoublePoint(distbig*Math.sin(angleFromTop+ang3), distbig*Math.cos(angleFromTop+ang3)));
+//            points.add(new DoublePoint(distsmall*Math.sin(angleFromTop+ang4), distsmall*Math.cos(angleFromTop+ang4)));
+//        }
+        for (int i = 0; i < 1000; i++) {
+            double ang = i*Math.PI/250;
+            double dist = 5+1*Math.sin(ang*20);
+            points.add(new DoublePoint(dist*Math.sin(ang), dist*Math.cos(ang)));
         }
         GliderPosition magnet = new GliderPosition(0, 0.5);
-        GliderPosition car = new GliderPosition(4, 0.5);
+        GliderPosition car = new GliderPosition(40, 0.5);
 
         Track t = new Track(points, car, magnet);
         new GraphicsPanel(t);
